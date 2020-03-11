76f0ca31a8e1ebb5b562a0b0b881ce16ec2b9dfe
grajoz <grajoz@yandex.ru>
2020-03-11 09:56:36 +0400

    feat: add revers-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  n=Math.abs(n);
+  n=String(n);
+  let arr2=[];
+  let arr=n.split("");
+  for (let i=0;i<arr.length;i++){
+      arr2[arr.length-i]=arr[i];
+  }
+  n=arr2.join("");
+  n=Number(n);
+  return n
 }
